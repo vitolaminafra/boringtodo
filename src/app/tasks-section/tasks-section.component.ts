552a669a5c2836cf4b509ec27149fb257c8b7419
Vito Laminafra <vitolaminafra@gmai..com>
2024-08-13 14:16:47 +0200

    Fix

--- src/app/tasks-section/tasks-section.component.ts
+++ src/app/tasks-section/tasks-section.component.ts
@@ -1,26 +1,20 @@
-import {Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Task } from '../../model/task';
-import {TaskViewComponent} from "../task-view/task-view.component";
-import {DividerModule} from "primeng/divider";
-import {NgIf} from "@angular/common";
-import { saveAs } from "file-saver";
-import {TaskService} from "../../service/task.service";
+import { TaskViewComponent } from '../task-view/task-view.component';
+import { DividerModule } from 'primeng/divider';
+import { NgIf } from '@angular/common';
+import { TaskService } from '../../service/task.service';
 
 @Component({
   selector: 'app-tasks-section',
   standalone: true,
-  imports: [
-    TaskViewComponent,
-    DividerModule,
-    NgIf
-  ],
+  imports: [TaskViewComponent, DividerModule, NgIf],
   templateUrl: './tasks-section.component.html',
-  styleUrl: './tasks-section.component.css'
+  styleUrl: './tasks-section.component.css',
 })
 export class TasksSectionComponent {
   @Input() taskData: Task[] = [];
   constructor(private taskService: TaskService) {}
-
 
   saveTask() {
     this.taskService.saveLocalTasks(this.taskData);
